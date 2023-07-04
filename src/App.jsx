7058ab48a498d1ecccbe98edf54746b6883b72d1
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-07-04 12:28:57 +0700

    animate spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import RoundOne from './components/roundeDesign/RoundOne'
+import RoundTwo from './components/roundeDesign/RoundTwo'
 
 function App() {
   return (
     <h1 className='w-full h-screen font-bodyFont text-textColor bg-black relative'>
-      <div className=''></div>
+      <div className='w-full h-full absolute top-0 left-0 z-10 '>
+        <RoundOne />
+        <RoundTwo />
+      </div>
     </h1>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
         titleRoundBg: 'linear-gradient(135deg, rgba(120,204,109,.15) 0%, rgba(120,204,109,1%) 100%)'
       },
       animation: {
-        'spin-slow ': 'spin 15s linear infinite',
+        'spin-slow': 'spin 15s linear infinite',
         'reverse-spin': 'reverse-spin 15s linear infinite'
       },
       keyframes: {
