3e9cf426d44b1cf34e59606cd6ccc1fcca652781
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-07-04 11:04:46 +0700

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='w-full h-screen font-bodyFont text-textColor bg-black relative'>
+      <div className=''></div>
+    </h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bodyFont: ['Poppins', 'sans-serif'],
+        titleFont: ['Montserrat', ' sans-serif']
+      },
+      colors: {
+        bodyColor: '#191919',
+        textColor: 'rgba(255,255,255,.85)',
+        designColor: '#edff20',
+        titleRoundBg: 'linear-gradient(135deg, rgba(120,204,109,.15) 0%, rgba(120,204,109,1%) 100%)'
+      },
+      animation: {
+        'spin-slow ': 'spin 15s linear infinite',
+        'reverse-spin': 'reverse-spin 15s linear infinite'
+      },
+      keyframes: {
+        'reverse-spin': {
+          from: {
+            transform: 'rotate(360deg)'
+          }
+        }
+      },
+      boxShadow: {}
+    }
+  },
+  plugins: []
+}
